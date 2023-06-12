604ce8e27dd3fb784209cba3a519da2f982994eb
Berk <brkmalhatun@gmail.com>
2023-06-13 02:36:05 +0300

    CreateDto

--- Company-service/src/main/java/com/sm/dto/request/CreateAppointmentRequestDto.java
+++ Company-service/src/main/java/com/sm/dto/request/CreateAppointmentRequestDto.java
@@ -1,2 +1,18 @@
-package com.sm.dto.request;public class CreateAppointmentRequestDto {
+package com.sm.dto.request;
+
+import com.sm.repository.entity.BaseEntity;
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CreateAppointmentRequestDto {
+        private Long companyId;
+        private String message;
 }
